6811a8c6b41c7ed348f89e0778cbe18d1b921798
rpalaparthi pscu.com <rpalaparthi@pscu.com>
2020-06-28 23:47:24 -0400

    add miration

--- Startup.cs
+++ Startup.cs
@@ -11,10 +11,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Advantage.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Advantage.API
 {
     public class Startup
-    {
+    { 
+        private string _connectionString=null;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +29,11 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            _connectionString = Configuration["secretConnectionString"];
             services.AddControllers();
+            services.AddEntityFrameworkNpgsql()
+            .AddDbContext<ApiContext>(
+                opt => opt.UseNpgsql(_connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
